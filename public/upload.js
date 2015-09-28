26d49cf9055c9150f70b1a8f020f740c9f07ce19
CRVV <crvv.pku@gmail.com>
2015-09-28 11:37:28 +0800

    0928

--- public/upload.js
+++ public/upload.js
@@ -1,43 +1,51 @@
-var UploadBox = React.createClass({
-    Uploader: function (file, url, box) {
-        var self = this;
-        this.name = file.name;
-        this.progress = 0;
-        this.request = new XMLHttpRequest();
-        this.cancel = function () {
-            self.request.abort();
-            self.done();
-        };
-        this.done = function () {
-            box.filesUploading.map(function (uploader, index) {
-                if (Object.is(uploader, self)) {
-                    box.filesUploading.splice(index, 1);
-                    box.refresh();
-                }
-            })
-        };
-        this.start = function () {
-            self.request.open('POST', url, true);
-            self.request.upload.addEventListener('progress', function (event) {
-                self.progress = event.loaded / event.total;
+function Uploader(file, url, box) {
+    var self = this;
+    this.name = file.name;
+    this.progress = 0;
+    this.request = new XMLHttpRequest();
+    this.cancel = function () {
+        self.request.abort();
+        self.done();
+    };
+    this.error = function () {
+        this.progress = -1;
+    };
+    this.done = function () {
+        box.filesUploading.map(function (uploader, index) {
+            if (Object.is(uploader, self)) {
+                box.filesUploading.splice(index, 1);
                 box.refresh();
-            });
-            self.request.upload.addEventListener('load', function (event) {
-                self.done();
-            });
-            var formData = new FormData();
-            formData.append(self.name, file);
-            self.request.send(formData);
-        };
-        this.start();
-    },
+            }
+        })
+    };
+    this.start = function () {
+        self.request.open('POST', url, true);
+        self.request.upload.addEventListener('progress', function (event) {
+            self.progress = event.loaded / event.total;
+            box.refresh();
+        });
+        self.request.upload.addEventListener('load', function (event) {
+            self.done();
+        });
+        self.request.upload.addEventListener('error', function (event) {
+            self.error();
+            box.refresh();
+        });
+        var formData = new FormData();
+        formData.append(self.name, file);
+        self.request.send(formData);
+    };
+    this.start();
+}
+
+var UploadBox = React.createClass({
     filesUploading: [],
     refresh: function () {
         this.setState({files: this.filesUploading});
     },
     handleUpload: function (files) {
         for (var i = 0; i < files.length; i++) {
-            this.filesUploading.push(new this.Uploader(files[i], this.props.url, this))
+            this.filesUploading.push(new Uploader(files[i], this.props.url, this))
         }
         this.refresh();
     },
@@ -84,7 +92,7 @@ var ProgressList = React.createClass({
 var Progress = React.createClass({
     render: function () {
         return (
-            <div>{this.props.file.name}: {this.props.file.progress}
+            <div>{this.props.file.name} :  {((this.props.file.progress) * 100).toFixed(0)} %
                 <input type="button" onClick={this.props.file.cancel} value="cancel"/>
             </div>
         )
